896e3220e8d50ac36274021e36d990c35093624f
Tyson Bailey <onaclov2000+github@gmail.com>
2017-08-24 07:05:52 -0600

    Update encode_and_join.py

--- encode_and_join.py
+++ encode_and_join.py
@@ -6,6 +6,8 @@ def filter_m4v(x):
 for dirs in sorted(os.listdir(".")):
    if os.path.isdir(dirs):
       os.chdir(dirs)
+      # Interstingly I was just thinking about this today, one could set it up so that each video was in some x time chunks
+      # Udacity doesn't split the videos into folders anymore which is super annoying.
       out = open('mylist.txt', 'w')
       for vids in sorted(os.listdir(".")):
          if '.mp4' in vids:
@@ -15,10 +17,11 @@ for dirs in sorted(os.listdir(".")):
             vids = tmp + '.mp4'
             out.write("file '" + vids[:-4] + ".m4v'\n")
             # if you don't need to re-encode just comment this line out, however
-            # OFTEN times the framerates are all wonky and this makes them all 25 so the audio/video *should
+            # OFTEN times the framerates are all wonky and this makes them all 25 fps so the audio/video *should
             # Stay in sync, with no weird pausing, also due to timestamp issues, re-encoding is necessary
             # otherwise the vidoes would say crazy inaccurate numbers for length of video.
             os.system('ffmpeg -i \"' + vids + '\" -c:v mpeg4 -b:v 1800k -c:a copy -vsync 1 -r 25 \"' + vids[:-4] + '.m4v\"')
+      
       out.close()
       os.system('ffmpeg -f concat -safe 0 -i mylist.txt -c copy \"../' + dirs + '.m4v\"')
       os.chdir('..')
