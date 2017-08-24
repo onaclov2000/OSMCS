b276b869a31d270b3a460f9c2fad695d489d66e8
Tyson Bailey <onaclov2000+github@gmail.com>
2017-08-24 05:23:37 -0600

    Update encode_and_join.py

--- encode_and_join.py
+++ encode_and_join.py
@@ -9,6 +9,10 @@ for dirs in sorted(os.listdir(".")):
       out = open('mylist.txt', 'w')
       for vids in sorted(os.listdir(".")):
          if '.mp4' in vids:
+            # remove non alpha-numeric characters by renaming for easier joining.
+            tmp = ''.join(e for e in vids[:-4] if e.isalnum())
+            os.rename(vids, tmp + '.mp4')
+            vids = tmp + '.mp4'
             out.write("file '" + vids[:-4] + ".m4v'\n")
             # if you don't need to re-encode just comment this line out, however
             # OFTEN times the framerates are all wonky and this makes them all 25 so the audio/video *should
